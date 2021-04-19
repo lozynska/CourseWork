9206dc1664690bafafe8da51248780127e89850c
lozynska <olha.vdovikovska@gmail.com>
2021-04-19 11:50:25 +0300

    add elements to startForm

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -41,6 +41,7 @@ namespace CourseWork
             this.button1.TabIndex = 0;
             this.button1.Text = "New Test";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -50,6 +51,7 @@ namespace CourseWork
             this.button2.TabIndex = 1;
             this.button2.Text = "Edit Test";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -16,5 +16,17 @@ namespace CourseWork
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form_New_Test form_New_Test = new Form_New_Test(); 
+            form_New_Test.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form_Edit form_Edit = new Form_Edit();
+            form_Edit.Show();
+        }
     }
 }
