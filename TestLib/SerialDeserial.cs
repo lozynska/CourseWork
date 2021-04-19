cb29e2e590fd55a5c47346a7719c74be336a28c2
lozynska <olha.vdovikovska@gmail.com>
2021-04-19 11:26:23 +0300

    add clas SerialiseDeserialize

---
+++ TestLib/SerialDeserial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TestLib
+{
+   public class SerialDeserial
+    {
+        public T Deserialize<T>(string input) where T : class
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T)); using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
